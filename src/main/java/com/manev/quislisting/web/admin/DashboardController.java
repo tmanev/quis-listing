ae98224c32370c1f72598754c0afd02f451756a7
Trajche Manev <trajche.manev@endava.com>
2017-01-22 22:44:30 +0100

    Starting html files for the admin part

---
+++ src/main/java/com/manev/quislisting/web/admin/DashboardController.java
@@ -0,0 +1,16 @@
+package com.manev.quislisting.web.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/ql-admin")
+public class DashboardController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String dashboardPage() {
+        return "admin/ql-admin";
+    }
+
+}
