7da1fb7201c60f4e825da7b1ddb2f369fedb7139
adrijana <adri.davitkova@gmail.com>
2017-02-13 20:33:53 +0100

    add services for save and delete

--- src/main/java/com/manev/quislisting/service/ContentFieldGroupService.java
+++ src/main/java/com/manev/quislisting/service/ContentFieldGroupService.java
@@ -2,13 +2,15 @@ package com.manev.quislisting.service;
 
 import com.manev.quislisting.domain.ContentFieldGroup;
 import com.manev.quislisting.repository.ContentFieldGroupRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ContentFieldGroupService {
-
+    private final Logger log = LoggerFactory.getLogger(ContentFieldGroupService.class);
     private ContentFieldGroupRepository contentFieldGroupRepository;
 
     public ContentFieldGroupService(ContentFieldGroupRepository contentFieldGroupRepository) {
@@ -20,6 +22,15 @@ public class ContentFieldGroupService {
     }
 
     public Page<ContentFieldGroup> findAll(Pageable pageable) {
+
         return contentFieldGroupRepository.findAll(pageable);
+    }
+    public ContentFieldGroup findOne(Long id) {
+        log.debug("Request to get PostCategory : {}", id);
+        return  contentFieldGroupRepository.findOne(id);
+    }
+    public void delete(Long id) {
+        log.debug("Request to delete PostCategory : {}", id);
+        contentFieldGroupRepository.delete(id);
     }
 }
