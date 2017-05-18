954da9e87b2af764af7a225e2613980426df5252
Trajche Manev <trajche.manev@gmail.com>
2017-05-18 11:41:27 +0200

    Contact DTO object added.

---
+++ src/main/java/com/manev/quislisting/service/dto/ContactDTO.java
@@ -0,0 +1,46 @@
+package com.manev.quislisting.service.dto;
+
+import javax.validation.constraints.NotNull;
+
+public class ContactDTO {
+    @NotNull
+    private String name;
+    @NotNull
+    private String email;
+
+    private String subject;
+
+    private String message;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
