cf1a2aba09bf396ded8a178a0a806d1dafb25f9a
Trajche Manev <trajche.manev@gmail.com>
2017-05-19 13:41:28 +0200

    sending contact us email

--- src/main/java/com/manev/quislisting/repository/EmailTemplateRepository.java
+++ src/main/java/com/manev/quislisting/repository/EmailTemplateRepository.java
@@ -7,5 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * Created by adri on 4/4/2017.
  */
 public interface EmailTemplateRepository extends JpaRepository<EmailTemplate, Long> {
+
+    EmailTemplate findOneByName(String name);
+
 }
 
--- src/main/java/com/manev/quislisting/service/EmailSendingService.java
+++ src/main/java/com/manev/quislisting/service/EmailSendingService.java
@@ -1,33 +1,62 @@
 package com.manev.quislisting.service;
 
+import com.manev.quislisting.domain.EmailTemplate;
 import com.manev.quislisting.domain.QlConfig;
+import com.manev.quislisting.repository.EmailTemplateRepository;
 import com.manev.quislisting.repository.QlConfigRepository;
 import com.manev.quislisting.service.dto.ContactDTO;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
+import com.manev.quislisting.service.util.StringAndClassLoaderResourceResolver;
+import org.apache.commons.lang3.CharEncoding;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.IContext;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.TemplateResolver;
 
 @Service
 public class EmailSendingService {
 
     private MailService mailService;
     private QlConfigRepository qlConfigRepository;
+    private SpringTemplateEngine springTemplateEngine;
 
-    public EmailSendingService(MailService mailService, QlConfigRepository qlConfigRepository) {
+    private EmailTemplateRepository emailTemplateRepository;
+
+    public EmailSendingService(MailService mailService, QlConfigRepository qlConfigRepository, EmailTemplateRepository emailTemplateRepository) {
         this.mailService = mailService;
         this.qlConfigRepository = qlConfigRepository;
+        this.emailTemplateRepository = emailTemplateRepository;
+
+        TemplateResolver resolver = new TemplateResolver();
+        resolver.setResourceResolver(new StringAndClassLoaderResourceResolver());
+        resolver.setPrefix("mail/"); // src/main/resources/mail
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode("HTML5");
+        resolver.setCharacterEncoding(CharEncoding.UTF_8);
+        resolver.setOrder(1);
+        this.springTemplateEngine = new SpringTemplateEngine();
+        this.springTemplateEngine.setTemplateResolver(resolver);
     }
 
     public void sendContactUs(ContactDTO contactDTO) {
-
         QlConfig adminEmailConfig = qlConfigRepository.findOneByKey("admin_email");
         if (adminEmailConfig == null) {
             throw new RuntimeException("Admin email not configured");
         }
 
-        String subject = String.format("Contact from %s", contactDTO.getName());
-        String content = "Subject: " + contactDTO.getSubject() + "\n" + contactDTO.getMessage();
+        EmailTemplate contactUsEmailTemplate = emailTemplateRepository.findOneByName("contact-us");
+        if (contactUsEmailTemplate == null) {
+            throw new RuntimeException("Email template not configured");
+        }
 
-        mailService.sendEmail(adminEmailConfig.getValue(), subject, content, false, true);
+        IContext context = new StringAndClassLoaderResourceResolver.StringContext(contactUsEmailTemplate.getText());
+        context.getVariables().put("name", contactDTO.getName());
+        context.getVariables().put("email", contactDTO.getEmail());
+        context.getVariables().put("subject", contactDTO.getSubject());
+        context.getVariables().put("message", contactDTO.getMessage().replaceAll("(\r\n|\n)", "<br />"));
+        context.getVariables().put("sendBy", "This e-mail was sent from a contact form on Quis Listing (http://quislisting.com)");
+        String actual = springTemplateEngine.process("redundant", context);
+
+        mailService.sendEmail(adminEmailConfig.getValue(), contactDTO.getSubject(), actual, false, true);
     }
 
 }
