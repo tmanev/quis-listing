81c0e8bec40c9d4b216dd49a51fec20c0130c840
Trajche Manev <trajche.manev@gmail.com>
2017-06-17 23:46:24 +0200

    MangedUser to supports updates flag

--- src/main/java/com/manev/quislisting/web/rest/vm/ManagedUserVM.java
+++ src/main/java/com/manev/quislisting/web/rest/vm/ManagedUserVM.java
@@ -24,11 +24,11 @@ public class ManagedUserVM extends UserDTO {
     }
 
     public ManagedUserVM(Long id, String login, String password, String firstName, String lastName,
-                         String email, boolean activated, String imageUrl, String langKey,
+                         String email, boolean activated, String imageUrl, String langKey, Boolean updates,
                          String createdBy, ZonedDateTime createdDate, String lastModifiedBy, ZonedDateTime lastModifiedDate,
                         Set<String> authorities) {
 
-        super(id, login, firstName, lastName, email, activated,  imageUrl, langKey,
+        super(id, login, firstName, lastName, email, activated,  imageUrl, langKey, updates,
             createdBy, createdDate, lastModifiedBy, lastModifiedDate,  authorities);
 
         this.password = password;
