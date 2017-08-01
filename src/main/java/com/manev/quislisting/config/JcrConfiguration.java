3bf6818559ae7677a1378a99e9604b394a651abe
Trajche Manev <trajche.manev@gmail.com>
2017-08-01 12:28:46 +0200

    - added staging profile - small refactor in JcrConfiguration - application to be build as jar

--- src/main/java/com/manev/quislisting/config/JcrConfiguration.java
+++ src/main/java/com/manev/quislisting/config/JcrConfiguration.java
@@ -1,48 +1,32 @@
 package com.manev.quislisting.config;
 
 import org.apache.jackrabbit.core.RepositoryImpl;
-import org.apache.jackrabbit.core.config.ConfigurationException;
 import org.apache.jackrabbit.core.config.RepositoryConfig;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
 
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 import javax.jcr.SimpleCredentials;
-import java.io.IOException;
 
 @Configuration
 public class JcrConfiguration {
 
     private static final String ADMIN = "admin";
-    private final ApplicationContext ctx;
     private final QuisListingProperties quisListingProperties;
 
-    private RepositoryConfig repositoryConfig;
     private RepositoryImpl repositoryImpl;
     private Session session;
 
-    public JcrConfiguration(ApplicationContext ctx, QuisListingProperties quisListingProperties) throws IOException, RepositoryException {
-        this.ctx = ctx;
+    public JcrConfiguration(QuisListingProperties quisListingProperties) throws RepositoryException {
         this.quisListingProperties = quisListingProperties;
         jcrRepoConfig();
     }
 
-//    @Bean
-    public RepositoryConfig jcrRepoConfig() throws RepositoryException, IOException {
-        Resource resource = ctx.getResource("classpath:repository.xml");
-        this.repositoryConfig = RepositoryConfig.create(resource.getFile().getPath(), quisListingProperties.getJcrRepository().getHome());
+    private void jcrRepoConfig() throws RepositoryException {
+        RepositoryConfig repositoryConfig = RepositoryConfig.create(this.getClass().getResource("/repository.xml").getPath(),
+                quisListingProperties.getJcrRepository().getHome());
         this.repositoryImpl = RepositoryImpl.create(repositoryConfig);
         this.session = repositoryImpl.login(new SimpleCredentials(ADMIN, ADMIN.toCharArray()));
-
-        return repositoryConfig;
-    }
-
-//    @Bean
-    public RepositoryImpl repository() throws RepositoryException, IOException {
-        return repositoryImpl;
     }
 
     public RepositoryImpl getRepositoryImpl() {
