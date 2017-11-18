0ff56cfe6a3ac0627afb4c61a21d985e4600cc2b
Trajche Manev <trajche.manev@gmail.com>
2017-11-18 20:08:32 +0100

    shutdown application gracefully

--- deployment_scripts/stop_quis_app.sh
+++ deployment_scripts/stop_quis_app.sh
@@ -4,7 +4,7 @@ QPID=`ps -ef | grep "server.port=$PORT" | grep -v grep | awk '{print $2}'`
 if [[ ! -z $QPID ]]; then
     QPID=`ps -ef | grep "server.port=$PORT" | grep -v grep | awk '{print $2}'`
     echo "About to kill gracefully process with pid: $QPID"
-    kill -8 $QPID
+    kill -15 $QPID
 else
     echo "Applicaion on port: $PORT is not active"
 fi
