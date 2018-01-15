e84f5f4652b277486005c9c6c8243613c743f65d
Trajche Manev <trajche.manev@gmail.com>
2018-01-15 22:15:38 +0100

    - front page items loaded initially from mvc then lazy loading added.

--- src/main/java/com/manev/quislisting/web/mvc/HomeController.java
+++ src/main/java/com/manev/quislisting/web/mvc/HomeController.java
@@ -5,9 +5,13 @@ import com.manev.quislisting.repository.qlml.LanguageRepository;
 import com.manev.quislisting.repository.qlml.LanguageTranslationRepository;
 import com.manev.quislisting.repository.taxonomy.NavMenuRepository;
 import com.manev.quislisting.service.QlConfigService;
+import com.manev.quislisting.service.post.DlListingService;
 import com.manev.quislisting.service.post.StaticPageService;
+import com.manev.quislisting.service.post.dto.DlListingDTO;
 import com.manev.quislisting.service.taxonomy.DlCategoryService;
 import org.springframework.context.MessageSource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,14 +27,16 @@ import java.util.Locale;
 public class HomeController extends BaseController {
 
     private DlCategoryService dlCategoryService;
+    private DlListingService dlListingService;
 
     public HomeController(NavMenuRepository navMenuRepository, QlConfigService qlConfigService,
                           StaticPageService staticPageService, LanguageRepository languageRepository,
                           LocaleResolver localeResolver,
-                          LanguageTranslationRepository languageTranslationRepository, MessageSource messageSource, DlCategoryService dlCategoryService) {
+                          LanguageTranslationRepository languageTranslationRepository, MessageSource messageSource, DlCategoryService dlCategoryService, DlListingService dlListingService) {
         super(navMenuRepository, qlConfigService, languageRepository, languageTranslationRepository, localeResolver,
                 staticPageService, messageSource);
         this.dlCategoryService = dlCategoryService;
+        this.dlListingService = dlListingService;
     }
 
     @RequestMapping(method = RequestMethod.GET)
@@ -38,6 +44,10 @@ public class HomeController extends BaseController {
         Locale locale = localeResolver.resolveLocale(request);
         String title = messageSource.getMessage("page.home.title", null, locale);
 
+        Page<DlListingDTO> page = dlListingService.findAllForFrontPage(new PageRequest(0, 12), locale.getLanguage());
+        model.addAttribute("dlListings", page.getContent());
+        model.addAttribute("totalDlListings", page.getTotalElements());
+        model.addAttribute("loadedDlListings", page.getNumberOfElements());
         List<CategoryCount> allCategoriesWithCount = dlCategoryService.findAllCategoriesWithCount(locale.getLanguage());
         model.addAttribute("dlCategories", allCategoriesWithCount);
 
--- src/main/webapp/resources/js/default.js
+++ src/main/webapp/resources/js/default.js
@@ -1,42 +1,57 @@
 LandingPage = {
-    init: function () {
+    init: function (totalDlListings, loadedDlListings) {
         Vue.use(window.vuelidate.default);
 
         var landingApp = new Vue({
             el: '#landingApp',
             data: {
-                tableParams: {
-                    page: {
-                        value: '1'
-                    },
-                    sort: {
-                        value: 'id,desc'
-                    },
+                pagingParams: {
+                    page: 1,
+                    sort: null,
                     search: null,
-                    url: '/api/public/dl-listings?page&sort&search'
+                    itemsPerPage: 12,
+                    totalDlListings: totalDlListings,
+                    loadedDlListings: loadedDlListings,
+                    url: '/api/public/dl-listings',
+                    isLoading: false
                 },
                 dlListings: []
             },
             validations: {},
-            methods: {},
-            created: function() {
-                this.$http({
-                    url: this.tableParams.url,
-                    method: 'GET'
-                }).then(function (response) {
-                    console.log('Success!:', response.data);
-                    // vm.links = ParseLinks.parse(headers.get('link'));
-                    let totalItems = response.headers.get('X-Total-Count');
-                    let queryCount = totalItems;
-                    this.dlListings = response.data;
-                }, function (response) {
-                    console.log('Error!:', response.data);
-                    $.notify({
-                        message: response.data
-                    }, {
-                        type: 'danger'
+            methods: {
+                onLoadNext: function () {
+                    this.pagingParams.isLoading = true;
+                    var $btn = $('#btnLoadMore').button('loading');
+                    this.$http({
+                        params: {
+                            page: this.pagingParams.page,
+                            size: this.pagingParams.itemsPerPage
+                        },
+                        url: this.pagingParams.url,
+                        method: 'GET'
+                    }).then(function (response) {
+                        console.log('Success!:', response.data);
+                        for (var i = 0; i<response.data.length; i++) {
+                            this.dlListings.push(response.data[i]);
+                        }
+                        this.pagingParams.page++;
+                        this.pagingParams.loadedDlListings += response.data.length;
+                        this.pagingParams.isLoading = false;
+                        $btn.button('reset');
+                    }, function (response) {
+                        console.log('Error!:', response.data);
+                        $.notify({
+                            message: response.data
+                        }, {
+                            type: 'danger'
+                        });
+                        this.pagingParams.isLoading = false;
+                        $btn.button('reset');
                     });
-                });
+                }
+            },
+            created: function() {
+
             }
         });
 
