1c2c182c6e27aa9d94321e11d296116a9a5f95a0
Trajche Manev <trajche.manev@gmail.com>
2018-02-25 02:28:35 +0100

    in case of error to show error message.

--- src/main/java/com/manev/quislisting/web/mvc/SignInController.java
+++ src/main/java/com/manev/quislisting/web/mvc/SignInController.java
@@ -2,7 +2,6 @@ package com.manev.quislisting.web.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,7 +13,7 @@ public class SignInController extends BaseController {
     @RequestMapping(value = MvcRouter.SIGN_IN)
     public String indexPage(final ModelMap model, Locale locale, HttpServletRequest request) {
         String error = request.getParameter("error");
-        if (!StringUtils.isEmpty(error)) {
+        if (error != null) {
             model.addAttribute("errMsg", messageSource.getMessage("page.signin.error.wrong_email_password", null, locale));
         }
         model.addAttribute("title", "Sign In");
